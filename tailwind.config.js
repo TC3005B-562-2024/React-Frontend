15bc40dda4b088fa999268f24519ff7f7a668e70
Diego Jacobo Martínez <diegojmr5@gmail.com>
2024-04-13 00:22:33 -0600

    Storybook added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 export default {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
+    fontFamily: {
+      'sans': ['Nunito', 'ui-sans-serif', 'system-ui'],
+    },
+    colors: {
+      'aci-orange': '#FCA311',
+    },
     extend: {},
   },
   plugins: [],
