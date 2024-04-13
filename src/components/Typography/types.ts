0952b5f5f18ee975b662bec431574d9871b04ade
Diego Jacobo Martínez <diegojmr5@gmail.com>
2024-04-13 00:24:15 -0600

    Typography component

---
+++ src/components/Typography/types.ts
@@ -0,0 +1,22 @@
+export interface ITypography {
+    /**
+     * The type (size) of the typography from largest to smallest:
+     * - banner
+     * - section title
+     * - title
+     * - text
+     */
+    type: 'banner' | 'section title' | 'title' | 'text'
+    /**
+     * The color of the text
+     */
+    color: 'black' | 'white' | 'red' | 'green' | 'blue' | 'yellow' | 'gray' | 'orange'
+    /**
+     * Whether the text should be bold or not
+     */
+    bold: boolean
+    /**
+     * The text to display
+     */
+    text: string
+}
