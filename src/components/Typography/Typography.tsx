cd7a8b28a42e7912d1d97e82646c507ebf1a1539
MoisesAdame <moisesadame01@gmail.com>
2024-04-15 18:17:27 -0600

    adding details, text type  to Typography

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -8,6 +8,7 @@ const Typography: React.FC<ITypography> = ({ type, color, bold, text }) => {
     'text-2xl': type === 'section title',
     'text-xl': type === 'title',
     'text-base': type === 'text',
+    'text-xs': type === 'detail',
     'text-black': color === 'black',
     'text-white': color === 'white',
     'text-red-600': color === 'red',
