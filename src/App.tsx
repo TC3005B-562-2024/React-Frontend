ad4479e2671d25c499ca646c48481d3c9b900ee0
MoisesAdame <moisesadame01@gmail.com>
2024-04-16 12:13:49 -0600

    Removing button instance.

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ function App() {
     <>
       <div className="text-red-600">Hola</div>
       <Icon iconName='star' color='red' filled />
-      <Button onClick={() => alert('Has presionado un botón!')} typo={{ text: 'Hola mundoooooooooooooooooo', type: 'title' }} color='orange' icon={{ iconName: 'star', filled: true }} />
     </>
   )
 }
