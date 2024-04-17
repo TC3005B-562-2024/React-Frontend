ad6198a4c2112c30600e0cbe3c63d709a2cbb109
Marco Iván <morkiglogxx@gmail.com>
2024-04-16 19:35:05 -0600

    LoginFormV1

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -8,7 +8,6 @@ const Typography: React.FC<ITypography> = ({ type, color, bold, text }) => {
     'text-2xl': type === 'section title',
     'text-xl': type === 'title',
     'text-base': type === 'text',
-    'text-xs': type === 'detail',
     'text-black': color === 'black',
     'text-white': color === 'white',
     'text-red-600': color === 'red',
@@ -20,36 +19,11 @@ const Typography: React.FC<ITypography> = ({ type, color, bold, text }) => {
     'font-bold': bold,
   });
 
-  // Function to format text with inline styles for highlighting
-  const formatText = (text: string) => {
-    // Assuming the text is provided in the format "[COLOR]Hellow World[/COLOR]"
-    const parts = text.split(/\[(.*?)\]/); 
-    let result: JSX.Element[] = [];
-    for (let i = 0; i < parts.length; i++) {
-      if (i % 2 === 0) {
-        // Normal text
-        result.push(<span key={i} className={classes}>{parts[i]}</span>);
-      } else {
-        // Highlighted text
-        const colorClass = `text-${parts[i]}-500`; // Assuming part is a valid color
-        result.push(<span key={i} className={`${classes} ${colorClass}`}>{parts[i + 1]}</span>);
-        i++; 
-      }
-    }
-    return result;
-  };
-
   return (
     <>
-      <div>{formatText(text)}</div>
+      <div className={classes}>{text}</div>
     </>
   )
 }
-
-Typography.defaultProps = {
-  type: 'text',
-  color: 'black',
-  bold: false,
-};
 
 export default Typography;
--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,38 +1,48 @@
 import React, { useState } from 'react';
 import './LoginForm.css'
 import Typography from '../Typography/Typography';
+import { Icon } from '../Icon';
 
 const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);  // Estado para controlar la visibilidad de la contraseña
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    // Handle login logic here
     console.log('Logging in with:', { email, password });
   }
 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);  // Cambia el estado de visibilidad de la contraseña
+  }
+
   return (
-    
-    <div className="">
-      <div className='  mb-2 text-center'><Typography type='section title' color='black' text='LOGIN' bold/></div>
-      <div className='  mb-10 text-center flex'><Typography type='text' color='black' text='Sign in with your [yellow]Amazon Connect[/yellow] credentials'/></div>
-      <div className=' px-6 '>
+    <div>
+      <div className='mb-2 text-center text-2xl font-bold'>LOGIN</div>
+      <div className='mb-10 text-center text-base'>Sign in with your <span className='text-yellow-500'>Amazon Connect</span> credentials</div>
+      <div className='px-6'>
         <form className="mx-auto max-w-md" onSubmit={handleSubmit}>
           <div className="mb-4">
-            <div className='mb-2 text-center'><Typography type='text' color='black' text='Email' bold/></div>
-            
+            <div className='mb-2 text-center text-base font-bold'>Email</div>
             <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email" required />
           </div>
 
           <div className="mb-11">
-            <div className='mb-2 text-center'><Typography type='text' color='black' text='Password' bold/></div>
-            <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="******" required />
+            <div className='mb-2 text-center text-base font-bold'>Password</div>
+            <div className='flex'>
+              <input type={showPassword ? "text" : "password"} id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter your password" required />
+              <span className=' flex  justify-around items-center  '>
+                <button type='button' onClick={togglePasswordVisibility} className='border-none cursor-pointer p-0  absolute mr-10  mt-2 '>
+                  {showPassword ? <Icon iconName='Visibility' color='yellow' filled /> : <Icon iconName='Visibility_off' color='yellow' filled />}
+                </button>
+              </span>
+            </div>
           </div>
 
           <div className="mb-6">
-            <button type="submit">
-              <Typography type='text' color='white' text='Login' bold/>
+            <button type="submit" className='submit'>
+              Login
             </button>
           </div>
         </form>
