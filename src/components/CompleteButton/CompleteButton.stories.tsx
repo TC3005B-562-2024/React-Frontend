854c726f67670525a98de29fc4fc09254bf07a07
Iván Hernández <102547531+JourneyHM@users.noreply.github.com>
2024-04-16 23:54:13 -0600

    Componente funcionando v.2  (#10)
    * Componente funcionando
    
    * Componente CompleteButton funcionando
    
    Aplicando los cambios y las consideraciones previstas

---
+++ src/components/CompleteButton/CompleteButton.stories.tsx
@@ -0,0 +1,33 @@
+import { Meta, StoryFn } from '@storybook/react';
+import { ICompleteButton } from './types';
+import CompleteButton from './CompleteButton';
+import React from 'react';
+
+const meta = {
+    title: 'Components/CompleteButton',
+    component: CompleteButton,
+    parameters: {
+        layout: "centered",
+    },
+    argTypes: {
+        isComplete: {control: 'boolean'},
+        color: {
+            options: ['black', 'white', 'red', 'green', 'blue', 'yellow', 'gray', 'orange'],
+            control: { type: 'select' },
+        },
+    },
+    tags: ["autodocs"],
+} satisfies Meta<typeof CompleteButton>
+
+export default meta;
+
+const Template: StoryFn<ICompleteButton> = (args) => <CompleteButton {...args} />;
+
+/**
+ * An complete button component to display a check mark or a radio button in case of a completed or incompleted action.
+ */
+export const Default = Template.bind({});
+Default.args = {
+    isComplete: true,
+    color: "green",
+};
---
+++ src/components/CompleteButton/CompleteButton.tsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import { ICompleteButton } from './types';
+import { Icon } from '../Icon';
+
+const CompleteButton: React.FC<ICompleteButton> = ({
+    color = 'green',
+    isComplete: isCompleteProp,
+    onToggleComplete,
+}) => {
+    const [isComplete, setIsComplete] = useState(isCompleteProp || false);
+
+    useEffect(() => {
+        setIsComplete(isCompleteProp || false);
+    }, [isCompleteProp]);
+
+    const handleButtonClick = () => {
+        const newValue = !isComplete;
+        setIsComplete(newValue);
+        if (onToggleComplete) {
+            onToggleComplete(newValue);
+        }
+    };
+
+    return (
+        <button onClick={handleButtonClick}>
+            <Icon iconName={isComplete ? 'check_circle' : 'radio_button_unchecked'} color={color} filled={isComplete} />
+        </button>
+    );
+};
+
+export default CompleteButton;
