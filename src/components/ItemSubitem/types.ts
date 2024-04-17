da30f1550ad2997056722d38a5b3d9429608de33
FranciscoSanchezz <117677307+FranciscoSanchezz@users.noreply.github.com>
2024-04-16 19:34:11 -0600

    Se agrego el componente de ItemSubitem, asi como su storie (#9)
    * Se agrego el componente de ItemSubitem, asi como su storie
    
    * Ya agregue el export en el index de components

---
+++ src/components/ItemSubitem/types.ts
@@ -0,0 +1,16 @@
+export interface IItemSubitem {
+    /**
+     * The title of the item
+     */
+    title: string;
+    /**
+     * The content of the item
+     */
+    content: string;
+    /**
+     * The color of the item
+     */
+    color: 'black' | 'red' | 'green' | 'yellow' | 'gray' ;
+
+}
+
