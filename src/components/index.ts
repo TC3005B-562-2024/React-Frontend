3d0bec90aadf8859fed9d15f062c98e406e7c4b6
JosueFuentesR12 <105233426+JosueFuentesR12@users.noreply.github.com>
2024-04-19 14:55:01 -0600

    AlertCard (#39)
    * AlertCard
    
    Terminado el AlertCard, con el botón funcionando
    
    * Corrección de errores
    
    Se corrigió:
    -tailwind
    -notación BEM
    -storybook
    -código
    
    * Corrección de errores
    
    Se ha modificado el código de acuerdo a lo solicitado
    
    * resolve comments
    
    * Fixed bad import
    
    ---------
    
    Co-authored-by: MoisesAdame <moisesadame01@gmail.com>
    Co-authored-by: Diego Jacobo Martínez <diegojmr5@gmail.com>

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { Pill } from './Pill';
 export { Icon } from './Icon';
 export { Button } from './Button';
+export { AlertCard } from './AlertCard';
 export { ItemSubitem } from './ItemSubitem';
 export { CompleteButton } from './CompleteButton';
 export { InformationBar } from './InformationBar';
