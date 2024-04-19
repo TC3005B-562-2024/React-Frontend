509d2519040553712106b588cba5b14ee927dfe9
MoisesAdame <moisesadame01@gmail.com>
2024-04-18 21:50:17 -0600

    centering icon in button

--- src/components/TrainingCardToggle/TrainingCardToggle.tsx
+++ src/components/TrainingCardToggle/TrainingCardToggle.tsx
@@ -8,7 +8,7 @@ const TrainingCardToggle: React.FC<ITrainingCardToggle> = ({
         <div className='bg-white box-content rounded-md shadow-md'>
             <div className='flex'>
                 <div className='flex-1 grow mx-4 my-4'>{label}</div>
-                <div className='flex-none w-5 my-4 mr-4'><CompleteButton isComplete color={color}/></div>
+                <div className='flex items-center my-4 mr-4'><CompleteButton isComplete color={color}/></div>
             </div>  
         </div>
     );
