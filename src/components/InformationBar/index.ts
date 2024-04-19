389357992294f9caba025ff86ef39be14e2adbb5
FranciscoSanchezz <A01656793@tec.mx>
2024-04-18 20:55:52 -0600

    Se agrego el componente InformationBar

---
+++ src/components/InformationBar/index.ts
@@ -0,0 +1,1 @@
+export { default as IInformationBar } from './InformationBar';
---
+++ src/components/InformationBar/types.ts
@@ -0,0 +1,13 @@
+import { IItemSubitem } from '../ItemSubitem/types';
+export interface IInformationBar {
+    /**
+     * The title of the information bar
+     */
+    title: string;
+    /**
+     * The elements of the information bar (the props of the ItemSubitem component)
+     */
+    elements: Array<{
+        itemsubitem: IItemSubitem;
+    }>
+}
