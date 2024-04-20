dad81519f6de63cdfd03048f068c60c555f6ee3d
ArturoCarballoArias <a01662245@tec.mx>
2024-04-19 23:50:39 -0600

    Cambio de classes

--- src/components/InsightDescription/InsightDescription.tsx
+++ src/components/InsightDescription/InsightDescription.tsx
@@ -2,28 +2,39 @@ import React from "react";
 import { IInsightDescription, PriorityType } from "./types";
 import { Icon } from "../Icon";
 import classNames from 'classnames';
+import './styles.css';
 
 const InsightDescription: React.FC<IInsightDescription> = ({ priority, alertId, description }) => {
-    const styles = getPriorityStyles(priority);
+    const alertClass = classNames({
+        'insight-description__container__alert-id__span--intervene': priority === 'intervene',
+        'insight-description__container__alert-id__span--transfer': priority === 'transfer',
+        'insight-description__container__alert-id__span--training': priority === 'training',
+    });
+
+    const priorityClass = classNames({
+        'insight-description__container__card--intervene': priority === 'intervene',
+        'insight-description__container__card--transfer': priority === 'transfer',
+        'insight-description__container__card--training': priority === 'training',
+      });
 
     return (
-        <div className="container">
+        <div className="w-full">
             {alertId && (
                 <div>
-                    <h1 className={"text-banner font-bold pb-3 insight-description__container__alert-id"}>
-                        Alert: <span className={`${styles.textColor}`}>{alertId}</span>
+                    <h1 className={"insight-description__container__alert-id"}>
+                        Alert: <span className={alertClass}>{alertId}</span>
                     </h1>
                 </div>
             )}
-            <div className={`flex border-solid border rounded-lg ${styles.bgColor} ${styles.borderColor} ${styles.textColor}`}>
-                <div className=" min-w-16 mx-6 insight-description__container__card__icon">
+            <div className={priorityClass}>
+                <div className="insight-description__container__card__icon">
                     {renderIcon(priority)}
                 </div>
-                <div className="p-2 insight-description__container__card__content">
-                    <h1 className="text-title capitalize insight-description__container__card__content__priority">
+                <div className="insight-description__container__card__content">
+                    <h1 className="insight-description__container__card__content__priority">
                         {priority}
                     </h1>
-                    <p className="text-text insight-description__container__card__content_description">
+                    <p className="insight-description__container__card__content_description">
                         {description}
                     </p>
                 </div>
@@ -44,34 +55,5 @@ const renderIcon = (priority: PriorityType) => {
             return <Icon iconName="warning" color="orange" />;
         default:
             return <Icon iconName="warning" color="orange" />
-    }
-}
-
-function getPriorityStyles(priority: PriorityType) {
-    switch (priority) {
-        case "intervene":
-            return {
-                bgColor: "bg-aci-red bg-opacity-20",
-                borderColor: "border-aci-red",
-                textColor: "text-aci-red"
-            };
-        case "transfer":
-            return {
-                bgColor: "bg-aci-orange bg-opacity-20",
-                borderColor: "border-aci-orange",
-                textColor: "text-aci-orange"
-            };
-        case "training":
-            return {
-                bgColor: "bg-[#F8C73E] bg-opacity-20",
-                borderColor: "border-[#F8C73E]",
-                textColor: "text-[#F8C73E]"
-            };
-        default:
-            return {
-                bgColor: "bg-gray-100",
-                textColor: "text-black",
-                borderColor: "border-gray-300"
-            };
     }
 }
