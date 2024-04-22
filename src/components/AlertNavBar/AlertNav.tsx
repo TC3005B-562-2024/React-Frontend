8a41e60bbcccfb5e53bde149b26b5e2d8f0e7346
SirPotato28 <A01662814@tec.mx>
2024-04-21 23:55:37 -0600

    MOVE

--- src/components/AlertNavBar/AlertNav.tsx
+++ src/components/AlertNavBar/AlertNav.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Icon } from '../Icon';
-import './AlertNav.css'; // Asegúrate de importar tu archivo de estilos CSS
+import './AlertNav.css'; // Make sure to import your CSS styles file
 
 /**
  * Card that shows important information of an alert.
@@ -18,19 +18,19 @@ const AlertNav: React.FC<AlertNavProps> = ({ instanceId, alertsExists }) => {
         <span className="font-bold text-lg text-white mr-2">INSTANCE:</span>
         <span className="text-orange-500">{instanceId}</span>
       </div>
-      <div className={`flex items-center ${alertsExists ? 'animate-shake' : ''}`}>
-        {/* Wrap the Icon component in a div and apply the 'shake' class conditionally */}
+      <div className={`flex items-center ${alertsExists ? 'animate-bounce' : ''}`}>
+        {/* Wrap the Icon component in a div and apply the 'bounce' class conditionally */}
         <div className={`relative ${alertsExists ? 'animate-wiggle' : ''}`}>
           <Icon 
             iconName="warning" 
             color={alertsExists ? "red" : "white"} 
-            className={`w-6 h-6 ${alertsExists ? 'animate-ping' : ''}`} 
+            className={`w-6 h-6 ${alertsExists ? 'animate-pulse' : ''}`} 
           />
         </div>
       </div>
-
     </nav>
   );
 };
+
 
 export default AlertNav;
