ddac1e35bfd5323dbf422c3da7d1db2ab438cefa
TektiticEel9062 <luismagpesc@gmail.com>
2024-04-30 01:49:56 -0600

    Loop infinito arreglado
    Se arregla el loop infinito, se debe modificar y poner el endpoint correcto

--- src/routes/PrivateRouter.tsx
+++ src/routes/PrivateRouter.tsx
@@ -5,10 +5,26 @@ import { AlertNav, SideBar } from '../components';
 const PrivateRouter = () => {
   const [skills, setSkills] = useState([]);
 
-  useEffect(() => {
+  /*useEffect(() => {
     console.error('PrivateRouter.tsx: Missing getSkills() implementation');
     setSkills([]);
-  }, [skills]);
+  }, [skills]);*/
+
+  useEffect(() => {
+    const fetchSkills = async () => {
+      
+      const fetchedSkills = await fetch('http://localhost:3000/skills') // TODO: Replace with actual endpoint
+        .then((response) => response.json())
+        .then((data) => data)
+        .catch((error) => {
+          console.error('PrivateRouter.tsx: fetchSkills() failed', error);
+          return [];
+        });
+      setSkills(fetchedSkills);
+    }
+    fetchSkills();
+  }, []); 
+
   
   return (
 
