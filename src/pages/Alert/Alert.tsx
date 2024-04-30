ccadf818ed75ff431e3a7af344181c2cd242ad8f
FranciscoSanchezz <A01656793@tec.mx>
2024-04-29 19:56:19 -0600

    Correcion de librerias sin usar

--- src/pages/Alert/Alert.tsx
+++ src/pages/Alert/Alert.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Button, InformationBar, InsightDescription} from '../../components';
 import { useParams } from 'react-router-dom';
 
