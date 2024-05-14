97ac45b3e9d1daff715d38ba3750e040bda122f9
JosueFuentesR12 <105233426+JosueFuentesR12@users.noreply.github.com>
2024-05-14 17:29:53 -0600

    inicios del componente
    Se creo css, tsx

--- src/components/HistoryAgent/HistoryAgent.tsx
+++ src/components/HistoryAgent/HistoryAgent.tsx
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Icon } from '../Icon';
+import { IIconNoColorNoSize } from "../Icon/types";
+import './HistoryAgent.css';
 
-const HistoryAgent: React.FC = () => {
+interface IHistoryAgentProps {
+  log: string;
+  date?: Date;
+  icon: IIconNoColorNoSize;
+  description: string;
+}
+
+const HistoryAgent: React.FC<IHistoryAgentProps> = ({ log, date, icon, description }) => {
+  const [expanded, setExpanded] = useState(false);
+  const [status, setStatus] = useState('');
+
+  const handlePanelClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const handleIconClick = () => {
+    setStatus(status === 'accepted' ? '' : 'accepted');
+  };
+
   return (
-    <div>
-      <h1>HistoryAgent</h1>
+    <div className={`history-agent__container ${status}`}>
+      <div className="history-agent__content" onClick={handlePanelClick}>
+        <div className="history-agent__left">
+          <div className="history-agent__log">{log}</div>
+        </div>
+        <div className="history-agent__right">
+        {date && ( // Verifica si date tiene un valor antes de intentar acceder a toDateString()
+            <div className="history-agent__date">{date.toDateString()}</div>
+        )}
+          <div className="history-agent__icon" onClick={handleIconClick}>
+            <Icon iconName={"check"} />
+            <span>{status === 'accepted' ? 'accepted' : 'ignored'}</span>
+          </div>
+        </div>
+      </div>
+      {expanded && (
+        <div className="history-agent__description">
+          {description}
+        </div>
+      )}
     </div>
   );
-}
+};
+
 export default HistoryAgent;
