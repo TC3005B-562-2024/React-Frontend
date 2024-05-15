f188186819df52cac76d780b278bf9d31bd2c193
JourneyHM <A01662556@tec.mx>
2024-05-15 17:24:17 -0600

    Base trainings para agent

---
+++ src/services/trainings/getTrainingsOfAgent.tsx
@@ -0,0 +1,33 @@
+import httpInstance from "../httpInstance";
+import { ITrainingsForAResponse } from "./types";
+
+export const getTrainingsOfAgent = async (id: number) => {
+    let res = null;
+    const endpoint = `/agents/${id}/trainings`;
+    await httpInstance
+        .get(endpoint)
+        .then((response) => {
+            res = response.data;
+        })
+        .catch((err) => {
+            console.error(err);
+            throw new Error("Failed to fetch alerts.");
+        });
+    return res as ITrainingsForAResponse | null;
+}
+
+/* Ejemplo de la estructura contemplada
+{
+    "id":0,
+    "results": [
+        {
+            "label": "Training Description",
+            "isComplete":false
+        },
+        {
+            "label": "Training Description",
+            "isComplete":true
+        }
+        ]
+    
+}*/
