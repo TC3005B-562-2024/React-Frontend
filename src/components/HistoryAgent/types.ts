b71836463d7ee6c6251243a3bdaabe885016138d
JosueFuentesR12 <105233426+JosueFuentesR12@users.noreply.github.com>
2024-05-14 18:04:17 -0600

    types
    srry

---
+++ src/components/HistoryAgent/types.ts
@@ -0,0 +1,8 @@
+import { IIconNoColorNoSize } from "../Icon/types";
+
+export interface IHistoryAgentProps {
+    log: string;
+    date: Date;
+    icon: IIconNoColorNoSize;
+    description: string;
+  }
