d1c966bea17bff8fd73c61bc0f471e829182d71e
MoisesAdame <moisesadame01@gmail.com>
2024-05-29 18:58:16 -0600

    Updating css and classnames

--- src/components/MainLoader/MainLoader.tsx
+++ src/components/MainLoader/MainLoader.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import './MainLoader.css'
+import React from 'react';
+import './MainLoader.css';
 
 export const MainLoader: React.FC = () => {
   return (
-  <div className="loop cubes">
-    <div className="item cubes"></div>
-    <div className="item cubes"></div>
-    <div className="item cubes"></div>
-    <div className="item cubes"></div>
-    <div className="item cubes"></div>
-    <div className="item cubes"></div>
-</div>
+    <div className="main-loader-loop main-loader-cube">
+      <div className="main-loader__item main-loader-cube"></div>
+      <div className="main-loader__item main-loader-cube"></div>
+      <div className="main-loader__item main-loader-cube"></div>
+      <div className="main-loader__item main-loader-cube"></div>
+      <div className="main-loader__item main-loader-cube"></div>
+      <div className="main-loader__item main-loader-cube"></div>
+    </div>
   )
-}
+};
 
-export default MainLoader
+export default MainLoader;
