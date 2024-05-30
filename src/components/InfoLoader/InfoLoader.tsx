fadd3fe3466a07d5746407536968b6f75e746388
MoisesAdame <moisesadame01@gmail.com>
2024-05-29 19:12:16 -0600

    Refactor of InfoLoader

--- src/components/InfoLoader/InfoLoader.tsx
+++ src/components/InfoLoader/InfoLoader.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import './InfoLoader.css';
 
-const InfoLoader: React.FC = ({
-}) => {
-    return (
-        <div className="loader">
-            <div className="bubble"></div>
-            <div className="bubble"></div>
-            <div className="bubble"></div>
-        </div>
+const InfoLoader: React.FC = () => {
+  return (
+    <div className="info-loader">
+      <div className="info-loader__bubble"></div>
+      <div className="info-loader__bubble"></div>
+      <div className="info-loader__bubble"></div>
+    </div>
 
-    )
-}
+  );
+};
 
 export default InfoLoader;
