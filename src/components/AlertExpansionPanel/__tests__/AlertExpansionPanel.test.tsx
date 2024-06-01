2a6428d015490d66d5efa94b0c07935789c9f8d8
Charles M <a01661896@tec.mx>
2024-05-31 19:30:30 -0600

    Update AlertExpansionPanel.test.tsx

--- src/components/AlertExpansionPanel/__tests__/AlertExpansionPanel.test.tsx
+++ src/components/AlertExpansionPanel/__tests__/AlertExpansionPanel.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, fireEvent, cleanup } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import AlertExpansionPanel from '../AlertExpansionPanel';
